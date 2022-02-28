c23a5942c0776980358bc8de44eb8046248bc352
Rashmi Singh <rashmi2030singh@gmail.com>
2022-02-28 11:37:55 +0530

    added the adding and delete functionality via props (parent-->subchildren)

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,72 @@ import React from 'react'
 import CartItem from './CartItem'
 
 class Cart extends React.Component{
+    constructor() {
+        super();
+        this.state= {
+            products : [
+                {
+                    title:'Mobile Phone',
+                    price:9999,
+                    qty:1,
+                    img:'',
+                    id:1
+                },
+                {
+                    title:'Watch',
+                    price:999,
+                    qty:1,
+                    img:'',
+                    id:2
+                },
+                {
+                    title:'Laptop',
+                    price:99999,
+                    qty:1,
+                    img:'',
+                    id:3
+                }
+            ]
+        }
+    }
+    handleIncreaseQuantity = (product) => {
+        // console.log('Hey please incre the qty of ',product)
+        const { products } = this.state;
+        const index = products.indexOf(product)
+        products[index].qty +=1;
+        this.setState({
+            products : products
+        });
+    }
+    handleDecreaseQuantity = (product) => {
+        // console.log('Hey please decre the qty of ',product)
+        const { products } = this.state;
+        const index = products.indexOf(product)
+        if(products[index].qty > 0){
+            products[index].qty -=1;
+            this.setState({
+                 products : products
+         });
+        }
+    }
     render(){
-        return(
+        return( 
             <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                
+               {
+                   this.state.products.map((product) => {
+                       return (
+                        <CartItem 
+                        product={product} 
+                        key={product.id}
+                        onIncreaseQuantity={this.handleIncreaseQuantity}
+                        onDecreaseQuantity={this.handleDecreaseQuantity}
+                        
+                      />
+                       )
+                       
+                   })
+               }
             </div>
         )
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,8 @@
 import React from 'react'
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state={
-            price:9999,
-            title: 'Mobile Phone',
-            qty:1,
-            img:''
-        }
-    }
+    
+    /*
     increaseQuantity = () =>{
         //console.log('this',this.state.qty);
         //setState form 1->
@@ -32,18 +25,20 @@ class CartItem extends React.Component {
                 }
             });
         }
-    }
+    }*/
 
     render() {
+        const product = this.props.product;
         return (
             <div className="cart-item">
+                 {/* { console.log(this.props) } */}
                  <div className="left-block">
                      <img alt="" style={style.image} />
                  </div>
                  <div className="right-block">
-                     <div style={{fontSize:25}}>{this.state.title}</div>
-                     <div style={{color:'#777'}}>Rs {this.state.price}</div>
-                     <div style={{color:'#777'}}>Qty: {this.state.qty}</div>
+                     <div style={{fontSize:25}}>{product.title}</div>
+                     <div style={{color:'#777'}}>Rs {product.price}</div>
+                     <div style={{color:'#777'}}>Qty: {product.qty}</div>
 
                      <div className="cart-item-actions">
                      {/* Buttons */}
@@ -51,12 +46,12 @@ class CartItem extends React.Component {
                      <img alt="increase"
                           className="action-icons"
                           src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1645718974~hmac=849f5241d3acde14400d0deaab813960"
-                          onClick={this.increaseQuantity}
+                          onClick={() => {this.props.onIncreaseQuantity(product)}}
                          />
                      <img alt="decrease"
                           className="action-icons"
                           src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
-                          onClick={this.decreaseQuantity}
+                          onClick={() => {this.props.onDecreaseQuantity(product)}}
                      />
                      <img alt="delete"
                           className="action-icons"
